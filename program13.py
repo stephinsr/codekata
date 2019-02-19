e1019b3eb7b274b438d73c55f8d56786dbbb0f82
stephinsr <47441448+stephinsr@users.noreply.github.com>
2019-02-19 11:32:41 +0530

    Update program13.py

--- program13.py
+++ program13.py
@@ -1,7 +1,7 @@
-a=int(input("Enter number: "))
+b=int(input("Enter number: "))
 k=0
-for i in range(2,a//2+1):
-    if(a%i==0):
+for i in range(2,b//2+1):
+    if(b%i==0):
         k=k+1
 if(k<=0):
     print("Number is prime")
