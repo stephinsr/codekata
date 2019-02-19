67add9e80fa8894aea492295966b7eeb8ae2eabe
stephinsr <47441448+stephinsr@users.noreply.github.com>
2019-02-19 13:45:44 +0530

    Update and rename program2.py to program20.py

--- program2.py
+++
@@ -1,7 +0,0 @@
-b=int(raw_input())
-if(b>0):
-  print("Positive")
-elif(b<0):
-  print("Negative")
-elif(b==0):
-  print("Zero")
---
+++ program20.py
@@ -0,0 +1,4 @@
+
+c=int(raw_input())
+for i in range(1,6):
+    print(i*c),
